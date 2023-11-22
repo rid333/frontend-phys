adbf235239c19555e0dc2792e1789294f355eacb
rid333 <alridho2003@gmail.com>
2023-11-22 10:14:38 +0800

    (6) updating graduate/undergraduate homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,7 +47,7 @@
         <div class="flex flex-col gap-y-6 absolute top-1/3 left-24 text-white z-10 w-2/5">
             <p class="text-4xl font-bold">Welcome to Hasanuddin University Department of Physics</p>
             <p class="text-lg font-medium text-slate100"><i>"Embark on a journey to unveil the true nature of reality, from the grandest cosmic scales to the tiniest subatomic particles"</i></p>
-            <Button class='w-fit font-medium bg-redUH-500 text-white hover:bg-redUH-600 active:bg-redUH-700 rounded-full'>
+            <Button class='w-fit bg-opacity-75 font-medium bg-redUH-500 text-white hover:bg-redUH-600 active:bg-redUH-700 rounded-full'>
                 <Play class='h-5 w-5 mr-2 font-light' strokeWidth=2 />Watch our Introduction Video
             </Button>
         </div>
@@ -63,7 +63,7 @@
     <div class="flex flex-col gap-y-8 mx-auto justify-center w-2/5">
         <div class="flex flex-col gap-y-4">
             <h2 class="scroll-m-20 text-3xl font-bold tracking-tight transition-colors first:mt-0">
-                Undergraduate Programs
+                Undergraduate Program
             </h2>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima dolores deleniti aut modi minus voluptatibus?</p>
             <Button class='w-fit font-medium bg-darkgreenUH-500 text-white hover:bg-darkgreenUH-600 active:bg-blueUH-700 rounded-lg'>
@@ -73,7 +73,7 @@
         <Separator class='bg-slate-400'/>
         <div class="flex flex-col gap-y-4">
             <h2 class="scroll-m-20 text-3xl font-bold tracking-tight transition-colors first:mt-0">
-                Graduate Programs
+                Graduate Program
             </h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio expedita laborum voluptas fugiat tenetur eligendi.</p>
             <Button class='w-fit font-medium bg-darkgreenUH-500 text-white hover:bg-darkgreenUH-600 active:bg-blueUH-700 rounded-lg'>
