86c13ebfe277f948a9c7c8229a603982548b2adf
rid333 <alridho2003@gmail.com>
2023-12-07 19:50:37 +0800

    (8) updated research areas

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -87,15 +87,15 @@
 
 
 <!-- Research Areas -->
-<div class="research-areas flex flex-col my-40 items-center m-auto text-white max-w-7xl">
-    <h1 class="italic text-5xl font-medium scroll-m-20 text-black underline mb-20">RESEARCH AREAS</h1>
+<div class="research-areas flex flex-col mt-40 items-center m-auto text-white max-w-7xl">
+    <h1 class="italic text-5xl font-semibold scroll-m-20 text-black underline mb-20">RESEARCH AREAS</h1>
     <div class="flex items-center justify-center">
         <div class="overflow-hidden">
             <img src="https://images.unsplash.com/photo-1690215230221-25d75d0cd6c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cG9ydHJhaXQlMjBzY2VuZXJ5fGVufDB8fDB8fHww" alt="" class="hover:scale-110 transition duration-500 cursor-pointer object-cover">
         </div>
-        <div class="box flex flex-col justify-center bg-redUH-400 p-14 w-2/3 gap-y-10 -ml-20 z-0">
+        <div class="box flex flex-col justify-center bg-redUH-400 p-14 w-2/3 gap-y-7 -ml-20 z-0">
             <h1 class="text-4xl font-light">Theoretical and Computation</h1>
-            <p class="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sequi omnis quod inventore impedit possimus quibusdam error. Debitis quam cumque aliquam, at sequi impedit suscipit, ipsa molestiae est inventore itaque vel quia dolor dolorem illum eligendi sapiente adipisci eaque, magnam quibusdam excepturi. Laborum qui quas, repudiandae totam exercitationem quia! Ratione autem quisquam omnis, vitae dolore iste amet veritatis et, excepturi illo cum numquam quia. Ea architecto commodi rem atque maiores, tempora minima perspiciatis? Veniam, repellendus.</p>
+            <p class="font-medium leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem sequi omnis quod inventore impedit possimus quibusdam error. Debitis quam cumque aliquam, at sequi impedit suscipit, ipsa molestiae est inventore itaque vel quia dolor dolorem illum eligendi sapiente adipisci eaque, magnam quibusdam excepturi. Laborum qui quas, repudiandae totam exercitationem quia! Ratione autem quisquam omnis, vitae dolore iste amet veritatis et, excepturi illo cum numquam quia. Ea architecto commodi rem atque maiores, tempora minima perspiciatis? Veniam, repellendus.</p>
             <a href="/" class="bg-white p-4 w-fit"><ExternalLink color='#D7251F' strokeWidth=2 /></a>
         </div>
     </div>
@@ -103,9 +103,9 @@
         <div class="overflow-hidden">
             <img src="https://images.unsplash.com/photo-1696737525743-8f81603b5ef0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cG9ydHJhaXQlMjBzY2VuZXJ5fGVufDB8fDB8fHww" alt="" class="hover:scale-110 transition duration-500 cursor-pointer object-cover">
         </div>
-        <div class="box flex flex-col justify-center bg-yellowUH-800 p-14 w-2/3 gap-y-10 -mr-20 z-0">
+        <div class="box flex flex-col justify-center bg-yellowUH-800 p-14 w-2/3 gap-y-7 -mr-20 z-0">
             <h1 class="text-4xl font-light">Material and Energy</h1>
-            <p class="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus impedit reiciendis, nobis ipsum a, nemo tempora et tenetur recusandae sint est unde architecto perferendis eos. Ea accusantium distinctio laudantium nostrum quisquam. Adipisci minima totam aliquam, impedit ea reprehenderit ad accusamus facilis inventore. Nesciunt vero sint, voluptates laudantium magnam repellendus saepe.</p>
+            <p class="font-medium leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus impedit reiciendis, nobis ipsum a, nemo tempora et tenetur recusandae sint est unde architecto perferendis eos. Ea accusantium distinctio laudantium nostrum quisquam. Adipisci minima totam aliquam, impedit ea reprehenderit ad accusamus facilis inventore. Nesciunt vero sint, voluptates laudantium magnam repellendus saepe.</p>
             <a href="/" class="bg-white p-4 w-fit"><ExternalLink color='#AEA04E' strokeWidth=2 /></a>
         </div>
     </div>
@@ -113,9 +113,9 @@
         <div class="overflow-hidden w-2/3">
             <img src="https://images.unsplash.com/photo-1657456553828-aef801dae56d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="hover:scale-110 transition duration-500 cursor-pointer object-cover">
         </div>
-        <div class="box flex flex-col justify-center bg-blueUH-500 p-14 w-2/3 gap-y-10 -ml-20 z-0">
+        <div class="box flex flex-col justify-center bg-blueUH-500 p-14 w-2/3 gap-y-7 -ml-20 z-0">
             <h1 class="text-4xl font-light">Electronics and Instrumentation</h1>
-            <p class="font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure sunt voluptatem fugit commodi? Quas odit autem aliquam. Suscipit ullam sunt perspiciatis ab dignissimos? A, dolorum. Repellat dignissimos fugiat ipsa et. Asperiores vel consectetur beatae ratione cumque quisquam quam? Repellendus, est numquam atque voluptate quaerat illum repellat? Aspernatur aliquam quod, quas, rem nesciunt voluptatibus quos, deserunt neque officia incidunt accusantium praesentium.</p>
+            <p class="font-medium leading-loose">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure sunt voluptatem fugit commodi? Quas odit autem aliquam. Suscipit ullam sunt perspiciatis ab dignissimos? A, dolorum. Repellat dignissimos fugiat ipsa et. Asperiores vel consectetur beatae ratione cumque quisquam quam? Repellendus, est numquam atque voluptate quaerat illum repellat? Aspernatur aliquam quod, quas, rem nesciunt voluptatibus quos, deserunt neque officia incidunt accusantium praesentium.</p>
             <a href="/" class="bg-white p-4 w-fit"><ExternalLink color='#010035' strokeWidth=2 /></a>
         </div>
     </div>
@@ -123,9 +123,9 @@
         <div class="overflow-hidden">
             <img src="https://images.unsplash.com/photo-1561394706-0f5b464eaa15?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fHBvcnRyYWl0JTIwc2NlbmVyeXxlbnwwfHwwfHx8MA%3D%3D" alt="" class="hover:scale-110 transition duration-500 cursor-pointer object-cover">
         </div>
-        <div class="box flex flex-col justify-center bg-darkgreenUH-500 p-14 w-2/3 gap-y-10 -mr-20 z-0">
+        <div class="box flex flex-col justify-center bg-darkgreenUH-500 p-14 w-2/3 gap-y-7 -mr-20 z-0">
             <h1 class="text-4xl font-light">Optics and Spectroscopy</h1>
-            <p class="font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam perferendis voluptatem quod nobis nostrum quibusdam est alias tempora praesentium, adipisci accusantium incidunt debitis neque reiciendis vitae iusto! Aspernatur, blanditiis. Optio cum excepturi ut unde, quia itaque quibusdam voluptates eveniet blanditiis?</p>
+            <p class="font-medium leading-loose">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam perferendis voluptatem quod nobis nostrum quibusdam est alias tempora praesentium, adipisci accusantium incidunt debitis neque reiciendis vitae iusto! Aspernatur, blanditiis. Optio cum excepturi ut unde, quia itaque quibusdam voluptates eveniet blanditiis?</p>
             <a href="/" class="bg-white p-4 w-fit"><ExternalLink color='#0D1F0F' strokeWidth=2 /></a>
         </div>
     </div>
@@ -133,6 +133,6 @@
 
 
 <!-- Recent News -->
-<div>
+<div class="news mt-40 font-semibold text-white max-w-7xl">
     <p class="italic text-5xl font-medium scroll-m-20 text-black underline mb-20">RECENT NEWS</p>
 </div>
